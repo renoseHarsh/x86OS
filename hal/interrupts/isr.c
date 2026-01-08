7a054ff2850e12b24c1f9fb9f626cdd066005c98
Harsh Renose <renoseharsh@gmail.com>
2026-01-08 17:52:03 +0530

    feat: introduce interrupt handler registration and dispatch

--- hal/interrupts/isr.c
+++ hal/interrupts/isr.c
@@ -2,6 +2,8 @@
 #include "isr.h"
 #include "kprintf.h"
 
+static isr_t interrupt_handlers[256] = { 0 };
+
 static const char *const g_Exceptions[] = { "Divide by zero error",
                                             "Debug",
                                             "Non-maskable Interrupt",
@@ -37,17 +39,27 @@ static const char *const g_Exceptions[] = { "Divide by zero error",
 
 void __attribute__((cdecl)) interrupt_handler(register_t *regs)
 {
+    if (interrupt_handlers[regs->interrupt]) {
+        interrupt_handlers[regs->interrupt](regs);
+    } else {
+        kprintf("\n\n");
+        kprintf(
+            "Unhandled exception %d %s\n", regs->interrupt,
+            g_Exceptions[regs->interrupt]
+        );
+        kprintf(
+            "eax=%x ebx=%x ecx=%x edx=%x esi=%x edi=%x\n", regs->ax, regs->bx,
+            regs->cx, regs->dx, regs->si, regs->di
+        );
+        kprintf("eip=%x cs=%x eflags=%x\n", regs->eip, regs->cs, regs->eflags);
+        kprintf(
+            "interrupt=%d error_code=%x\n", regs->interrupt, regs->error_code
+        );
+        kernel_panic();
+    }
+}
 
-    kprintf("\n\n");
-    kprintf(
-        "Unhandled exception %d %s\n", regs->interrupt,
-        g_Exceptions[regs->interrupt]
-    );
-    kprintf(
-        "eax=%x ebx=%x ecx=%x edx=%x esi=%x edi=%x\n", regs->ax, regs->bx,
-        regs->cx, regs->dx, regs->si, regs->di
-    );
-    kprintf("eip=%x cs=%x eflags=%x\n", regs->eip, regs->cs, regs->eflags);
-    kprintf("interrupt=%d error_code=%x\n", regs->interrupt, regs->error_code);
-    kernel_panic();
+void register_interrupt_handler(uint8_t n, isr_t handler)
+{
+    interrupt_handlers[n] = handler;
 }
