92396a7099bd533a8b9ec15ba9687c139569724c
Harsh Renose <renoseharsh@gmail.com>
2026-01-08 17:50:35 +0530

    feat: connect IDT entries to new ISR stubs

--- hal/interrupts/idt.c
+++ hal/interrupts/idt.c
@@ -1,19 +1,10 @@
 #include "idt.h"
-#include "kprintf.h"
 #include "string.h"
 #include <stdint.h>
 
 __attribute__((aligned(0x10))) static idt_entry idt[256];
 static idt_ptr idt_descriptor;
-
-void exception_handler()
-{
-    kprintf("Exception occurred!\n");
-    __asm__ volatile("cli");
-
-    while (1)
-        __asm__ volatile("hlt");
-}
+extern void *isr_stub_table[]; // Pointer to the array of ISR stubs in isr.asm
 
 static void set_idt_entry(uint8_t vector, void *isr, uint8_t attributes)
 {
@@ -29,8 +20,8 @@ static void set_idt_entry(uint8_t vector, void *isr, uint8_t attributes)
 void init_idt()
 {
     kmemset(&idt, 0, sizeof(idt_entry) * 256);
-    for (int i = 0; i < 256; i++) {
-        set_idt_entry(i, exception_handler, 0x8E);
+    for (int i = 0; i < 32; i++) {
+        set_idt_entry(i, isr_stub_table[i], 0x8E);
     }
 
     idt_descriptor.limit = sizeof(idt_entry) * 256 - 1;
