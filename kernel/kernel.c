4b7d132aaad3e96172f54945b5d3e3d476bfbf57
Harsh Renose <renoseharsh@gmail.com>
2026-01-09 23:28:18 +0530

    chore: initialize PIC and enable IRQs

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,14 +1,25 @@
 #include "idt.h"
+#include "isr.h"
 #include "kprintf.h"
+#include "pic.h"
 #include "vga.h"
 #include <stdbool.h>
 #include <stdint.h>
+
+void timer(register_t *regs)
+{
+    (void)regs;
+}
+
 int kmain()
 {
     vga_clear();
     init_idt();
     kprintf("IDT initialized.\n");
-
+    init_pic();
+    kprintf("PIC initialized.\n");
+    register_interrupt_handler(32, timer);
+    __asm__ volatile("sti");
     for (;;) {
     }
 }
