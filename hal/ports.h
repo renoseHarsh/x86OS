dbf22af486332224ec392bfd56ed245db3c3838b
Harsh Renose <renoseharsh@gmail.com>
2026-01-09 23:20:15 +0530

    refactor: inline port I/O helpers

--- hal/ports.h
+++ hal/ports.h
@@ -2,5 +2,19 @@
 
 #include <stdint.h>
 
-void outb(uint16_t port, unsigned char val);
-unsigned char inb(uint16_t port);
+static inline void outb(uint16_t port, uint8_t val)
+{
+    __asm__ volatile("outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
+}
+
+static inline uint8_t inb(uint16_t port)
+{
+    uint8_t ret;
+    __asm__ volatile("inb %w1, %b0" : "=a"(ret) : "Nd"(port) : "memory");
+    return ret;
+}
+
+static inline void io_wait(void)
+{
+    outb(0x80, 0);
+}
