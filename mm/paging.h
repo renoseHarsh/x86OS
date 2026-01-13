31869201a091dfb3b7dd924cd43ce6740de0426e
Harsh Renose <renoseharsh@gmail.com>
2026-01-14 01:08:22 +0530

    chore: define higher half memory layout
    - Update linker script to map kernel at 3GB
    - Add P2V and V2P macros
    - Add layout constants (KERNEL_VIRTUAL_BASE)

--- mm/paging.h
+++ mm/paging.h
@@ -5,6 +5,11 @@
 #define PAGE_SIZE 4096
 #define PAGE_MASK 0xFFFFF000
 
+#define KERNEL_VIRTUAL_BASE 0xC0000000
+
+#define P2V(addr) ((uint32_t)(addr) + KERNEL_VIRTUAL_BASE)
+#define V2P(addr) ((uint32_t)(addr) - KERNEL_VIRTUAL_BASE)
+
 #define PAGE_PRESENT 0x1
 #define PAGE_RW 0x2
 #define PAGE_USER 0x4
