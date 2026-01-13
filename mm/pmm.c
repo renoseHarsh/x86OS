93ba0ef0b7d4062d0862a49946da4d5b2aa118a8
Harsh Renose <renoseharsh@gmail.com>
2026-01-14 01:17:32 +0530

    refactor: adapt subsystems to virtual addressing
    - Adjust PMM to handle the new virtual _kernel_end symbol
    - Utilize P2V/V2P macros for physical memory translation
    - Update VGA driver to write to video memory via 3GB window

--- mm/pmm.c
+++ mm/pmm.c
@@ -1,3 +1,4 @@
+#include "paging.h"
 #include "pmm.h"
 #include "string.h"
 #include <stdint.h>
@@ -5,12 +6,12 @@
 extern char _kernel_end[];
 
 // No need to align, as _kernel_end is already page-aligned
-static uint32_t placement_addr = (uint32_t)_kernel_end;
+static uint32_t placement_addr = (uint32_t)V2P(_kernel_end);
 
 void *pmm_alloc_page()
 {
     void *page = (void *)placement_addr;
     placement_addr += 0x1000; // Advance by one page (4KB)
-    kmemset(page, 0, 0x1000);
+    kmemset((void *)P2V(page), 0, 0x1000);
     return page;
 }
