14f6e1f755414c89f927bc91e6ef3e7cda76d191
Harsh Renose <renoseharsh@gmail.com>
2026-01-29 23:29:48 +0530

    feat: implement granular kernel section and permission mapping

--- mm/vmm.c
+++ mm/vmm.c
@@ -1,5 +1,5 @@
+#include "layout.h"
 #include "paging.h"
-#include "pmm.h"
 #include "string.h"
 #include "vmm.h"
 #include <stdint.h>
@@ -77,8 +77,7 @@ void vmm_init()
 {
     kmemset(page_directory, 0, sizeof(page_directory));
     page_directory[1023] = V2P(page_directory) | PAGE_PRESENT | PAGE_RW;
-    map_range(
-        0, KERNEL_VIRTUAL_BASE, 0x40000 - 0x1000, PAGE_PRESENT | PAGE_RW
-    );
+    map_bios();
+    map_kernel();
     refresh_cr3();
 }
