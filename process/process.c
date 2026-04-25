600bdd394be4e6b784633c3cdd9a5f6bb9854e2f
Harsh Renose <renoseharsh@gmail.com>
2026-04-26 01:52:03 +0530

    feat: implement user process creation with schedulable thread

--- process/process.c
+++ process/process.c
@@ -1,15 +1,50 @@
 #include "elf.h"
+#include "heap/heap.h"
 #include "kprintf.h"
 #include "layout.h"
 #include "paging.h"
 #include "pmm.h"
 #include "process.h"
+#include "sched.h"
 #include "string.h"
+#include "thread.h"
 #include <stddef.h>
 #include <stdint.h>
 
 extern pde_t kernel_page_directory[];
 extern void jump_usermode(uintptr_t eip);
+extern size_t thread_id;
+
+typedef struct {
+    uint32_t kernel_stack_base;
+    uint32_t kernel_esp;
+} KERNEL_STACK_INFO;
+
+KERNEL_STACK_INFO setup_kernel_stack(uintptr_t user_esp, uintptr_t entry)
+{
+
+    uint32_t *stack_base = kmalloc(0x1000);
+    uint32_t *esp = &stack_base[1024];
+
+    *--esp = (4 << 3 | 0 | 3); // SS
+    *--esp = user_esp;
+    *--esp = (1 << 9);         // eflags interrupt enabled
+    *--esp = (3 << 3 | 0 | 3); // CS
+    *--esp = entry;            // eip
+
+    *--esp = 0; // error code
+    *--esp = 0; // interrupt number
+
+    for (int i = 0; i < 8; i++) {
+        *--esp = 0;
+    } // eax, ecx, edx, ebx, esp, ebp, esi, edi
+
+    KERNEL_STACK_INFO info;
+    info.kernel_stack_base = (uint32_t)stack_base;
+    info.kernel_esp = (uint32_t)esp;
+
+    return info;
+}
 
 void create_process(void *elf_ptr)
 {
@@ -24,21 +59,24 @@ void create_process(void *elf_ptr)
     }
 
     uint32_t *user_stack = (uintptr_t *)P2V(pmm_alloc(0));
+    uintptr_t user_esp = stack_base + 0x1000;
+    uintptr_t entry = ((Elf32_Ehdr *)elf_ptr)->e_entry;
 
     map_range(
         process_pd, V2P(user_stack), stack_base, 1,
         PAGE_PRESENT | PAGE_USER | PAGE_RW
     );
 
-    uint32_t *sp = &user_stack[1024];
-    uintptr_t user_esp = (uintptr_t)sp;
+    KERNEL_STACK_INFO info = setup_kernel_stack(user_esp, entry);
 
-    *--sp = (uint32_t)(4 << 3 | 0 | 3);
-    *--sp = stack_base + PAGE_SIZE;
-    *--sp = (1 << 9);
-    *--sp = (uint32_t)(3 << 3 | 0 | 3);
-    *--sp = 0x40000000;
+    Thread *thread = kmalloc(sizeof(Thread));
+    thread->que.next = NULL;
+    thread->que.prev = NULL;
+    thread->id = thread_id++;
+    thread->status = RUNNABLE;
+    thread->kernel_stack_base = info.kernel_stack_base;
+    thread->kernel_esp = info.kernel_esp;
+    thread->pd = process_pd;
 
-    refresh_cr3(process_pd);
-    jump_usermode((uintptr_t)sp);
+    sched_enqueue(thread);
 }
