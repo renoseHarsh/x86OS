1618fa2e4a5c0d32118148640e719686e62411ff
Harsh Renose <renoseharsh@gmail.com>
2026-05-08 04:57:50 +0530

    refactor: load ELF segments into allocated pages instead of direct mapping

--- process/elf.c
+++ process/elf.c
@@ -17,28 +17,33 @@ uintptr_t load_segment(
     if (cur->p_type != PT_LOAD)
         return 0x0;
 
-    uintptr_t vaddr = cur->p_vaddr;
-    uintptr_t paddr = V2P((uintptr_t)elf_hdr + cur->p_offset);
-
-    size_t filesz_pages = ALIGN_UP(cur->p_filesz, PAGE_SIZE) >> 12;
-    size_t memsz_pages = ALIGN_UP(cur->p_memsz, PAGE_SIZE) >> 12;
-    size_t zero_pages = memsz_pages - filesz_pages;
-
-    uintptr_t zero_vaddr = vaddr + (filesz_pages << 12);
-
     uint32_t flags = PAGE_PRESENT | PAGE_USER;
     if (cur->p_flags == (PF_R | PF_W))
         flags |= PAGE_RW;
 
-    if (filesz_pages)
-        map_range(pd, paddr, vaddr, filesz_pages, flags);
+    uintptr_t vaddr = cur->p_vaddr;
+    uint8_t *elf_src_ptr = ((uint8_t *)elf_hdr) + cur->p_offset;
 
-    for (size_t i = 0; i < zero_pages; i++) {
-        uintptr_t zero = (uintptr_t)pmm_alloc(0);
-        map_range(pd, zero, zero_vaddr + (i << 12), 1, flags);
-    }
+    size_t pages_needed = ALIGN_UP(cur->p_memsz, PAGE_SIZE) >> 12;
 
-    return phdrs[idx].p_vaddr + (memsz_pages << 12);
+    size_t copy_remaining = cur->p_filesz;
+
+    for (size_t i = 0; i < pages_needed; i++) {
+        void *cur_page = pmm_alloc(0);
+        void *kernel_vir_cur_page = (void *)P2V(cur_page);
+
+        kmemset(kernel_vir_cur_page, 0, PAGE_SIZE);
+        size_t copying = copy_remaining;
+        if (copy_remaining) {
+            copying
+                = (copy_remaining > PAGE_SIZE) ? PAGE_SIZE : copy_remaining;
+            kmemcpy(kernel_vir_cur_page, elf_src_ptr + (i << 12), copying);
+            copy_remaining -= copying;
+        }
+        map_range(pd, (uintptr_t)cur_page, vaddr + (i << 12), 1, flags);
+    };
+
+    return cur->p_vaddr + (pages_needed << 12);
 }
 
 uintptr_t load_elf(pde_t *pd, Elf32_Ehdr *elf_hdr)
